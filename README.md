a0d3a0537dc4973b67a2e3fcacf8c4f76d94c142
muaz <muaz.vakitsayan@gmail.com>
2022-07-02 07:17:43 +0300

    updated

---
+++ README.md
@@ -0,0 +1,6 @@
+## React FAQ with Accordion
+
+I'm trying to use a different method in each React project. I used the toggle structure in this project. In addition, when the buttons are pressed, the icons change while opening. useState is a must for this project because we control the changes in our page with state.
+***
+[Click Here To See All Project](https://react-accordionfaq.netlify.app/)
+![]()
--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,19 +1,26 @@
-import React, {useState} from 'react';
-import { AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai';
+import React, { useState } from "react";
+import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
 
-const Faq = ({title,info}) => {
-    const [toggle, setToggle] = useState(false)
-    
+const Faq = ({ title, info }) => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <div>
-        <h3 className='title'><button className='btn' onClick={() => setToggle(!toggle)}>{toggle ?<AiFillMinusCircle className='icon'/>: <AiFillPlusCircle className='icon'/>}</button>{title}</h3>
-        <h4 className='parag'>{toggle && info}
-        <div></div>
-        
-        </h4>
-        
+      <h3 className="title">
+        <button className="btn" onClick={() => setToggle(!toggle)}>
+          {toggle ? (
+            <AiFillMinusCircle className="icon" />
+          ) : (
+            <AiFillPlusCircle className="icon" />
+          )}
+        </button>
+        {title}
+      </h3>
+      <h4 className="parag">
+        {toggle && info}
+      </h4>
     </div>
-  )
-}
+  );
+};
 
 export default Faq;
